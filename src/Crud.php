85df982b5fc4b23bc30b2ea6deb08702ea2e7ec6
Hans Kuijpers <info@hkweb.nl>
2016-11-09 09:33:31 +0100

    return early
    I was inspired by the presentation of @rdohms and try to write code that lasts.
    
    http://www.slideshare.net/rdohms/writing-code-that-lasts-magento2seminar-utrecht slide 42

--- src/Crud.php
+++ src/Crud.php
@@ -126,12 +126,18 @@ class Crud
     {
         $id = (empty($this->variables[$this->pk])) ? $id : $this->variables[$this->pk];
 
-        if (!empty($id)) {
-            $sql = "SELECT * FROM " . $this->table . " WHERE " . $this->pk . "= :" . $this->pk . " LIMIT 1";
-
-            $result = $this->db->row($sql, array($this->pk => $id));
-            $this->variables = ($result != false) ? $result : null;
+        if (empty($id)) {
+            return false;
         }
+        
+        $sql = "SELECT * FROM " . $this->table . " WHERE " . $this->pk . "= :" . $this->pk . " LIMIT 1";
+        $result = $this->db->row($sql, array($this->pk => $id));
+        
+        if($result == false) {
+            return false;
+        }
+      
+        return $this->variables = $result;
     }
 
     /**
